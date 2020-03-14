d8ded4c06735cc162022a13bc15d2bd70479e0f3
laijay <jack1011tw@gmail.com>
2020-03-14 20:06:06 +0800

    finish Q28

--- 28-word-count.rb
+++ 28-word-count.rb
@@ -1,5 +1,16 @@
 # 请打开 wordcount.txt，计算每个单字出现的次数
 
+file = File.open("wordcount.txt")
 doc = File.read("wordcount.txt")
 
-# ...
+arr = doc.split(" ")
+h = {}
+
+arr.each do |v|
+  if h[v] == nil
+    h[v] = 1
+  else
+    h[v] += 1
+  end
+end
+puts h
