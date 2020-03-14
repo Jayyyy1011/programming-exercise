b566a8f440cfa44d8b2e7ebb026d7dc7934d1d69
laijay <jack1011tw@gmail.com>
2020-03-14 17:17:08 +0800

    finish Q19

--- 19-filter.rb
+++ 19-filter.rb
@@ -1,7 +1,11 @@
 # 给定一阵列内含数字，输出另一个数组只包含偶数
 
 def filter_even(arr)
-  #...
+  arr.each_with_index do |i, j|
+    if i % 2 != 0
+      arr.delete_at(j)
+    end
+  end
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
