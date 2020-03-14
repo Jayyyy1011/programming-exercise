9dba3071420a3e380f78311826ca8f4d022fcf34
laijay <jack1011tw@gmail.com>
2020-03-14 15:08:08 +0800

    finish Q13

--- 13-nn.rb
+++ 13-nn.rb
@@ -3,9 +3,15 @@
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
-# while ( ... )
-#   while ( ...)
-#
-#   end
-# end
+i = 0
+j = 0
 
+ while i <= n.to_i
+   while j <= n.to_i
+     answer = i * j
+     puts "#{i} x #{j} = #{answer}"
+     j += 1
+   end
+   i += 1
+   j = 0
+ end
