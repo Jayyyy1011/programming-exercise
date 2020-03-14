4254268e8211af2d8a0e6d8c8f40e229dd5500db
laijay <jack1011tw@gmail.com>
2020-03-14 14:03:37 +0800

    finish Q6

---
+++ 06-integer-positive.rb
@@ -0,0 +1,22 @@
+# 题目: 输入一个数字 x，请判断是否正数、零或负数，以及是不是偶数
+
+
+print "请输入一个整数，然后按 Enter: "
+x = gets
+
+if x.to_i  > 0
+  result = "正數"
+elsif x.to_i < 0
+  result = "負數"
+else
+  result = "零"
+end
+
+if x.to_i % 2 != 0
+  result2 = "奇數"
+else
+  result2 = "偶數"
+end
+
+puts "这个数是#{result}"
+puts "这个数是#{result2}"
